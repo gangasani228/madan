5048395a4218f19ca1817153274ebd27c00e802e
gangasani228 <madan@LAPTOP-0EIQM5N8.localdomain>
2020-03-17 19:25:13 +0530

    cmp 2 f str

---
+++ tfile.py
@@ -0,0 +1,13 @@
+with open("file.txt","r") as f:
+    data=f.read()
+    k=data.split("\n")
+    print(k)
+print("###################################")
+with open("file1.txt","r") as f1:
+    data1=f1.read()
+    k1= data1.split("\n")
+    print(k1)
+    f1.close()
+for s in k:
+    if s in k1:
+        print(s)
